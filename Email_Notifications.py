9d3947d1680b2c51432fcd455561e4efd2098066
stung168 <stung168@users.noreply.github.com>
2017-06-28 14:06:29 -0400

    Creating/Sending emails methods added

---
+++ Email_Notifications.py
@@ -0,0 +1,100 @@
+from apiclient import discovery
+from apiclient import errors
+from oauth2client import client, tools
+from oauth2client.file import Storage
+from email.mime.text import MIMEText
+
+import base64, googleapiclient, httplib2, os
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/googleapis.com-python.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.modify'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Rates'
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+    The credentials are saved at the directory ~/.credentials/googleapis.com-python.json
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print("Storing credentials to " + credential_path)
+    return credentials
+
+def initialize_gmail_service():
+    """Initializes a Gmail service instance.
+    
+    Returns:
+        gmail_service, a Gmail service instance.
+    """        
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    gmail_service = discovery.build('gmail', 'v1', http=http)
+    
+    return gmail_service
+
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+def send_message(user_id, message):
+    """Send an email message.
+
+    Args:
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      message: Message to be sent.
+
+    Returns:
+      Sent Message.
+    """
+    gmail_service = initialize_gmail_service() # gmail_service: Authorized Gmail API service instance.
+    
+    try:
+        message = (gmail_service.users().messages().send(userId=user_id, body=message)
+               .execute())
+        print 'Message Id: %s' % message['id']
+        return message
+    except errors.HttpError, error:
+        print 'An error occurred: %s' % error
